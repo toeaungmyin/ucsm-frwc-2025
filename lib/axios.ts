48b091dc50f89c33ded2e0ee133aaa133194d1e9
toeaungmyin <toeaungmyin.official@gmail.com>
2024-12-19 21:12:04 +0630

    fix axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+    baseURL: `https://ucsm-frwc-2024-2025.vercel.app/api`,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
